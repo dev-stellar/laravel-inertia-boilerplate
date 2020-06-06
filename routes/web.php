d5d3b3e1bd68a07401588410d1ba1d1ef2099ae5
Mosab Ibrahim <miaababikir@gmail.com>
2020-06-07 00:29:41 +0200

    Disable register and reset auth

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
+
+Auth::routes(['register' => false, 'confirm' => false, 'reset' => false]);
 
 
 Route::middleware('auth')->prefix('/dashboard')->group(function () {
