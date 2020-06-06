5b3816ccd2c27559e272a440ee1a29856eb03021
Mosab Ibrahim <miaababikir@gmail.com>
2020-06-06 22:03:56 +0200

    Intalling dashboard preset

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('base-nav', require('./components/UI/BaseNav').default);
+Vue.component('base-header', require('./components/UI/BaseHeader').default);
+Vue.component('base-panel', require('./components/UI/BasePanel').default);
+Vue.component('base-input', require('./components/UI/BaseInput').default);
+Vue.component('base-sidebar', require('./components/UI/BaseSidebar').default);
+
+const app = new Vue({
+    el: '#app',
+});
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::view('/ui', 'ui.index');
